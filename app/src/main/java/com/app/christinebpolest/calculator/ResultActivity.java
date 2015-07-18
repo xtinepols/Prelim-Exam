f716b67ce6c904c1b91814f4b370f22e29cd3a1c
xtinepols <xtinepolestico@gmail.com>
2015-07-18 10:08:44 +0800

    Final

--- app/src/main/java/com/app/christinebpolest/calculator/ResultActivity.java
+++ app/src/main/java/com/app/christinebpolest/calculator/ResultActivity.java
@@ -3,6 +3,7 @@ package com.app.christinebpolest.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
